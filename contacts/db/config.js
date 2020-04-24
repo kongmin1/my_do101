29ae83202121f40f7b37ec40089ac163b469ee00
kongmin1 <min.kong@usdoj.gov>
2020-04-24 13:36:19 -0400

    added log

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const DB_CONFIG = process.env.DB_CONFIG;
 const { Pool } = require('pg');
-
-const pgconn = new Pool({
+console.log('DB_CONFIG===='+DB_CONFIG);
+const pgconn = new Pool({    
     connectionString: DB_CONFIG,
     ssl: false,
 });
