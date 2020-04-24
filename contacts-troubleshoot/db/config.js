b4946f82cacffe41e4f0418fbddac3734ee09945
kongmin1 <min.kong@usdoj.gov>
2020-04-24 17:08:18 -0400

    added log

--- contacts-troubleshoot/db/config.js
+++ contacts-troubleshoot/db/config.js
@@ -7,7 +7,13 @@ var db_user = process.env["database-user"];
 var db_pass = process.env["database-password"];
 var db_name = process.env["database-name"];
 
+console.log('db_user===='+db_user);
+console.log('db_pass===='+db_pass);
+console.log('db_name===='+db_name);
+
 const DB_CONFIG = util.format('postgresql://%s:%s@%s:%s/%s', db_user, db_pass, process.env.POSTGRESQL_SERVICE_HOST, process.env.POSTGRESQL_SERVICE_PORT, db_name);
+
+console.log('DB_CONFIG===='+DB_CONFIG);
 
 const { Pool } = require('pg');
 
